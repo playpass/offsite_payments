ef8f4b11dc59a17a592d8f9db2acd3138bd1baf3
Shahbaz Javeed <sjaveed@gmail.com>
2014-09-24 02:30:28 +0400

    * Added signed_field_names and unsigned_field_names fields

--- lib/offsite_payments/integrations/cyber_source_secure_acceptance_sop.rb
+++ lib/offsite_payments/integrations/cyber_source_secure_acceptance_sop.rb
@@ -137,6 +137,8 @@ module OffsitePayments #:nodoc:
         end
 
         def insert_fixed_fields
+          add_field('signed_field_names', 'access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency')
+          add_field('unsigned_field_names', '')
           add_field('locale', 'en')
           add_field('transaction_uuid', SecureRandom.hex(16))
           add_field('sendMerchantURLPost', 'true')
